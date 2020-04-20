2a3945342d0e413cb323123cb6568b0e9429273d
Hans Kristian Smedsrød <eplehans@gmail.com>
2020-04-20 10:12:20 +0200

    added some navigation smartness

---
+++ js/main.js
@@ -0,0 +1,33 @@
+$('nav a').on('click', function() {
+
+    var scrollAnchor = $(this).attr('data-scroll'),
+        scrollPoint = $('section[data-anchor="' + scrollAnchor + '"]').offset().top - 28;
+
+    $('body,html').animate({
+        scrollTop: scrollPoint
+    }, 500);
+
+    return false;
+
+})
+
+
+$(window).scroll(function() {
+    var windscroll = $(window).scrollTop();
+    if (windscroll >= 400) {
+        $('nav').addClass('sticky');
+
+        $('section[data-anchor]').each(function(i) {
+            if ($(this).position().top <= windscroll + 80) {
+                $('nav a.header__nav__link--active').removeClass('header__nav__link--active');
+                $('nav a').eq(i).addClass('header__nav__link--active');
+            }
+        });
+
+    } else {
+        $('nav').removeClass('sticky');
+        $('nav a.header__nav__link--active').removeClass('header__nav__link--active');
+        //$('nav a:first').addClass('header__nav__link--active');
+    }
+
+}).scroll();
