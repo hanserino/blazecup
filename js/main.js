df72eef4f053811a5284b70ecb9e0e2d7a2f2815
Hans Kristian Smedsrød <eplehans@gmail.com>
2020-04-20 11:51:18 +0200

    mobile stuff

--- js/main.js
+++ js/main.js
@@ -1,33 +1,35 @@
-$('nav a').on('click', function() {
 
-    var scrollAnchor = $(this).attr('data-scroll'),
-        scrollPoint = $('section[data-anchor="' + scrollAnchor + '"]').offset().top - 28;
+if ( $(window).width() > 700 ) {
+    $('nav a').on('click', function() {
 
-    $('body,html').animate({
-        scrollTop: scrollPoint
-    }, 500);
+        var scrollAnchor = $(this).attr('data-scroll'),
+            scrollPoint = $('section[data-anchor="' + scrollAnchor + '"]').offset().top - 28;
+    
+        $('body,html').animate({
+            scrollTop: scrollPoint
+        }, 500);
+    
+        return false;
+    
+    })
 
-    return false;
+    $(window).scroll(function() {
+        var windscroll = $(window).scrollTop();
+        if (windscroll >= 400) {
+            $('nav').addClass('sticky');
 
-})
+            $('section[data-anchor]').each(function(i) {
+                if ($(this).position().top <= windscroll + 80) {
+                    $('nav a.header__nav__link--active').removeClass('header__nav__link--active');
+                    $('nav a').eq(i).addClass('header__nav__link--active');
+                }
+            });
 
+        } else {
+            $('nav').removeClass('sticky');
+            $('nav a.header__nav__link--active').removeClass('header__nav__link--active');
+            //$('nav a:first').addClass('header__nav__link--active');
+        }
 
-$(window).scroll(function() {
-    var windscroll = $(window).scrollTop();
-    if (windscroll >= 400) {
-        $('nav').addClass('sticky');
-
-        $('section[data-anchor]').each(function(i) {
-            if ($(this).position().top <= windscroll + 80) {
-                $('nav a.header__nav__link--active').removeClass('header__nav__link--active');
-                $('nav a').eq(i).addClass('header__nav__link--active');
-            }
-        });
-
-    } else {
-        $('nav').removeClass('sticky');
-        $('nav a.header__nav__link--active').removeClass('header__nav__link--active');
-        //$('nav a:first').addClass('header__nav__link--active');
-    }
-
-}).scroll();
+    }).scroll();
+}
